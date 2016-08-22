296aba1eee6e78a0f5e44816056e8f7a5ee86212
LINTA-CHUNG <BarryLin@LINTA-CHUNGde-MacBook-Air.local>
2016-08-22 20:47:52 +0800

    commit

--- tdoh.js
+++ tdoh.js
@@ -8,7 +8,27 @@ jQuery(document).ready(function(){
 		return false;
 	});
 
-	jQuery(document).tooltip({
+	jQuery('.tooltip-enable').tooltip({
 		track: true
+	});
+
+	jQuery('#program table').magnificPopup({
+		delegate: 'td.link',
+		mainClass: 'mfp-zoom-in',
+		callbacks: {
+		    beforeOpen: function() {
+		       jQuery('#mfp-popup-program .avator').html( jQuery(this.st.el).find('.avatar').html() );
+		       jQuery('#mfp-popup-program .data-title').text( jQuery(this.st.el).find('.title').text() );
+		       jQuery('#mfp-popup-program .data-description').html( jQuery(this.st.el).find('.description').html() );
+		       jQuery('#mfp-popup-program .data-speaker').text( jQuery(this.st.el).find('.speaker').text() );
+		    },
+		    close: function() {
+		       jQuery('#mfp-popup-program .avator').html('');
+		       jQuery('#mfp-popup-program .data-title').text('');
+		       jQuery('#mfp-popup-program .data-description').html('');
+		       jQuery('#mfp-popup-program .data-speaker').text('');
+		    }
+		},
+		midClick: true 
 	});
 });
